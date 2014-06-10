ec96c5dd66d8db047e0e0ba2aa2bce10e0a46ba1
Frank Puhlmann <frank@frapu.de>
2014-06-10 20:59:14 +0200

    Added Carsten Braune as a contributor.

--- src/com/inubit/research/gui/WorkbenchAboutDialog.java
+++ src/com/inubit/research/gui/WorkbenchAboutDialog.java
@@ -73,7 +73,7 @@ public class WorkbenchAboutDialog extends javax.swing.JDialog {
         jTextArea1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jTextArea1.setLineWrap(true);
         jTextArea1.setRows(5);
-        jTextArea1.setText("(C) 2009-2014 Contributors\n\nContributors: \nFelix Elliger, Fabian Friedrich, Uwe Hartmann, Thomas Milde, Jan Oehlert, Frank Puhlmann, Johannes Schmidt\n\nThis software contains the following external components:\n\n1. ProcessEditor (C) 2008, 2009 Frank Puhlmann (http://frapu.de)\n2. JSONObject (C) 2002 JSON.org\n");
+        jTextArea1.setText("(C) 2009-2014 Contributors\n\nContributors: \nCarsten Braune, Felix Elliger, Fabian Friedrich, Uwe Hartmann, Thomas Milde, Jan Oehlert, Frank Puhlmann, Johannes Schmidt\n\nThis software contains the following external components:\n\n1. ProcessEditor (C) 2008, 2009 Frank Puhlmann (http://frapu.de)\n2. JSONObject (C) 2002 JSON.org\n");
         jTextArea1.setWrapStyleWord(true);
         jScrollPane1.setViewportView(jTextArea1);
 
